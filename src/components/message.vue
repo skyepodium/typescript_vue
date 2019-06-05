a98d8110b31f4702b651afc215458763ca9756b7
skyepodium <skyepodium@gmail.com>
2019-06-05 16:47:49 +0900

    first commit

---
+++ src/components/message.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <input type="" v-model="message">    
+        <div>{{ message }}</div>
+    </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+
+@Component // 클래스 인스턴스를 컴포넌트 인스턴스로 사용하기 위함
+export default class Message extends Vue {
+    message: string = '메세지를 입력해주세요'
+}
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <message />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
+// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import message from '@/components/message.vue' //@를 붙여주면 src로 인식된다. 
 @Component({
   components: {
-    HelloWorld,
+    message
   },
 })
 export default class Home extends Vue {}
